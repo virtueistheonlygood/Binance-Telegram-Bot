7ca64e828e8c00d577086136e175e4da89296c19
Karan <kkrahar15@gmail.com>
2021-06-15 20:32:26 +0530

    initial commit

--- GSheetsAPI.py
+++ GSheetsAPI.py
@@ -1,11 +1,18 @@
+from Binance_Bot_old import readFile
 import pandas as pd
 import pygsheets
 import Sheets_Config as sh
 
 
+fileName = readFile("sheets.json")["fileName"]
+spot = readFile("sheets.json")["spotSheet"]
+gem = readFile("sheets.json")["gemSheet"]
+futures = readFile("sheets.json")["futuresSheet"]
+scalp = readFile("sheets.json")["scalpSheet"]
+
 def getSheet(SHEET):
     gc = pygsheets.authorize()
-    sht = gc.open('Crypto')
+    sht = gc.open(fileName)
     wk1 = sht.worksheet_by_title(SHEET)
     df = pd.DataFrame(wk1.get_all_records())
     return df
@@ -15,14 +22,14 @@ def getSheet(SHEET):
 def getAllSheets():
 
     gc = pygsheets.authorize()
-    sht = gc.open('Crypto')
+    sht = gc.open(fileName)
     # sh.SPOT = pd.DataFrame(sht.worksheet_by_title('SPOT'))
     # sh.FUTURES = pd.DataFrame(sht.worksheet_by_title('FUTURES'))
     # sh.SCALP = pd.DataFrame(sht.worksheet_by_title('SCALP'))
     # sh.GEM = pd.DataFrame(sht.worksheet_by_title('GEM'))
-    sh.SPOT = getSheet('SPOT')
-    sh.FUTURES = getSheet('FUTURES')
-    sh.SCALP = getSheet('SCALP')
-    sh.GEM = getSheet('GEM')
+    sh.SPOT = getSheet(spot)
+    sh.FUTURES = getSheet(futures)
+    sh.SCALP = getSheet(scalp)
+    sh.GEM = getSheet(gem)
     # print(sh.SPOT)s
     # return [SPOT, FUTURES, SCALP, GEM]
